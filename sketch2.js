1a8a2bb26ee32902cdce1ce28c5927645bdf7095
livongithub <berul526@newschool.edu>
2023-02-17 16:37:10 -0500

    commit

--- sketch2.js
+++ sketch2.js
@@ -1,20 +1,48 @@
-const cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-}
-)
+// const cursor = document.querySelector('.cursor');
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+// }
+// )
 
-const cursor1 = document.querySelector('.cursor1');
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-}
-)
+// const cursor1 = document.querySelector('.cursor1');
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+// }
+// )
+
+    const cursorTag= document.querySelector("div.cursors")
+    const balls= cursorTag.querySelectorAll("div")
+
+    let aimX = 0 
+    let aimY = 0
+
+    balls.forEach((ball, index) => {
+        let currentX = 0 
+        let currentY = 0
+      
+        let speed = 0.2 - index * 0.02
+
+    const animate = function () {
+        currentX += (aimX - currentX) * speed
+        currentY += (aimY - currentY) * speed
+
+        ball.style.left = currentX + "px"
+        ball.style.top = currentY + "px"
+
+        requestAnimationFrame(animate)
+    }
+
+    animate()
+    })
+
+   
 
 
-function fadeOut() {
-    const fade = document.getElementById('shadow');
+    
 
-    fade.style.opacity = '75';
-}
+document.addEventListener("mousemove", function (event) {
+    aimX = event.pageX
+    aimY = event.pageY
+} )
---
+++ sketch3.js
@@ -0,0 +1,13 @@
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+}
+)
+
+const cursor1 = document.querySelector('.cursor1');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+}
+)
